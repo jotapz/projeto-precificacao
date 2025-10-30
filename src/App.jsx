75f4109b29bcf07fa9c86c41a65d6f39f45aade3
João Pedro <jpedros1511@gmail.com>
2025-10-30 18:29:14 -0300

    testando

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home.jsx'
 
 function App() {
 
   return (
-   <h1 class="text-primary">
-    TESTE BOOTSTRAP
-    </h1>
+    
   )
 }
 
