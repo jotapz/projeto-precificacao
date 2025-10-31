85b7136ac2a4bcbefe72b9b223d8b0e9b985618b
João Pedro <jpedros1511@gmail.com>
2025-10-30 21:11:31 -0300

    feature: components, pages, routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Inicio from './pages/Inicio.jsx'
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
 
   return (
-   <h1 class="text-primary">
-    TESTE BOOTSTRAP
-    </h1>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/CardPrecificacao.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function CardPrecificacao() {
+  return (
+    <div className="card border-info mb-3" style={{ maxWidth: "18rem" }}>
+      <div className="card-header d-flex align-items-center justify-content-between">
+        Header
+        {/* ícone via classe bi (garanta import bootstrap-icons no main.jsx) */}
+        <i
+          className="bi bi-info-circle"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="Esta é uma dica sobre o produto!"
+          style={{ cursor: "pointer" }}
+        ></i>
+      </div>
+      <div className="card-body text-info">
+        <h5 className="card-title">Materia</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CardPrecificacao;
